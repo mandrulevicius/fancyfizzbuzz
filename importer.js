81ecd716e063f70e34153e5dc6bb07dd48eec41f
Mindaugas Andrulevicius <mindaugas.andrulevicius@gmail.com>
2023-07-02 11:42:11 +0300

    move imports into separate file

---
+++ importer.js
@@ -0,0 +1,27 @@
+'use strict';
+
+import fs from 'fs';
+
+// IO function - file system!
+async function importFeatures(featureFlags) {
+  const features = {};
+  for (const name in featureFlags) if (featureFlags[name] === true) features[name] = {};
+  for (const name in features) features[name] = await importFiles(name);
+  return features;
+}
+
+async function importFiles(name) {
+  const feature = {};
+  const fileNames = fs.readdirSync(`./features/${name}`);
+  const jsFileNames = fileNames.filter(fileName => fileName.endsWith('.js'));
+  for (const jsFileName of jsFileNames) {
+    feature[truncExtension(jsFileName, '.js')] = await import(`./features/${name}/${jsFileName}`);
+  }
+  return feature;
+}
+
+function truncExtension(fileName, extension) {
+  return fileName.slice(0, fileName.length - extension.length);
+}
+
+export default { importFeatures };
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
 
-import fs from 'fs';
+import importer from './importer.js';
 import tester from './tester.js';
 // REMINDER - COMMIT MORE OFTEN
+
+// instead of just extracting code into functions, maybe should extract the function into setup?
+// if a function is calling another function without it having been passed in, that is a side effect
 
 // first, test when customInputs feature is missing.
 //  should work fine since the flag is off
@@ -11,22 +14,13 @@ import tester from './tester.js';
 
 const featureFlags = {
   main: true,
-  customInputs: true
+  customInputs: false
 };
-const features = {};
-// TODO extract into function:
-for (const name in featureFlags) if (featureFlags[name] === true) features[name] = {};
-for (const name in features) {
-  const fileNames = fs.readdirSync(`./features/${name}`);
-  const jsFileNames = fileNames.filter(fileName => fileName.endsWith('.js'));
-  for (const jsFileName of jsFileNames) {
-    features[name][jsFileName.slice(0, jsFileName.length - 3)] = await import(`./features/${name}/${jsFileName}`);
-  }
-}
-
+const features = await importer.importFeatures(featureFlags);
 const results = await tester.runPrelaunchTests(features);
 console.log('testResults:', results);
 if (results.pass) launch();
+// turn off failing features, run tests again
 
 function launch() {
   const SIZE = 100;
@@ -35,7 +29,7 @@ function launch() {
   outputFizzBuzz(features.main.fizzBuzz.solveFizzBuzz(SIZE, DIV1, DIV2));
 }
 
-// IO function!
+// IO function - output to user!
 function outputFizzBuzz(outputList) {
   console.log(outputList);
 }
