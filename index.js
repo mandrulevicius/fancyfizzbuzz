69c5ef0bf3a56ef94346d0fc109e293c0562231c
Mindaugas Andrulevicius <mindaugas.andrulevicius@gmail.com>
2023-07-04 16:30:57 +0300

    refactor test isolation logic

--- index.js
+++ index.js
@@ -5,30 +5,30 @@ import tester from './tester.js';
 // REMINDER - COMMIT MORE OFTEN
 
 // instead of just extracting code into functions, maybe should extract the function into setup?
-// if a function is calling another function without it having been passed in, that is a side effect
+
+// if a function is calling another function without it having been passed in, that is not ideal
+// not sure what would gain from passing around all functions.
 
 const featureFlags = {
   main: true,
   customInputs: true
 };
-const features = await importer.importFeatures(featureFlags);
-const results = await tester.runPrelaunchTests(features);
-console.log('TestResults:', results); // log results
 
-// TODO refactor
-if (results.pass) launch(features);
-else {
-  // should check if results.pass is false or null before deleting.
-  // will do nothing if no features anyway
-  delete results.pass;
-  const isolatedFeatureFlags = {};
-  for (const featureName in results) isolatedFeatureFlags[featureName] = results[featureName].pass;
-  const isolatedFeatures = await importer.importFeatures(isolatedFeatureFlags);
-  const isolatedResults = await tester.runPrelaunchTests(isolatedFeatures);
-  console.log('IsolatedTestResults:', isolatedResults); // log results
-  if (isolatedResults.pass) launch(isolatedFeatures);
-  // if isolatedResults fail, should break loudly
+const results = await init(featureFlags);
+if (results.pass) return;
+console.log('Some tests failed, isolating passing features');
+const isolatedFeatureFlags = extractIsolatedFeatureFlags(results);
+const isolatedResults = init(isolatedFeatureFlags);
+if (!isolatedResults.pass) return console.log('ISOLATED FEATURES DID NOT PASS TESTS!');
+
+async function init(featureFlags) {
+  const features = await importer.importFeatures(featureFlags);
+  const results = await tester.runPrelaunchTests(features);
+  console.log('TestResults:', results); // log results
+  if (results.pass) launch(features);
+  return results.pass;
 }
+// will need to deal with console logs
 
 function launch(features) {
   const SIZE = 100;
@@ -41,6 +41,15 @@ function launch(features) {
 // IO function - output to user!
 function outputFizzBuzz(outputList) {
   console.log(outputList);
+}
+
+function extractIsolatedFeatureFlags(results) {
+  delete results.pass;
+  // should check if results.pass is false or null before deleting.
+  // or will do nothing if no features anyway?
+  const isolatedFeatureFlags = {};
+  for (const featureName in results) isolatedFeatureFlags[featureName] = results[featureName].pass;
+  return isolatedFeatureFlags;
 }
 
 // logger
